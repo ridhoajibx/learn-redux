b6e9a63b1472e06e1651d8a359712899faae48cd
ridhoajibx <ridhoajibx@gmail.com>
2020-10-29 02:22:42 +0700

    [ridho] refactor function rootReducer

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,8 +1,13 @@
+import { ADD_ARTICLE } from "../constant/action-type";
+
 const initialState = {
     articles: []
 };
 
 function rootReducer ( state = initialState, action ) {
+    if (actiom.type === ADD_ARTICLE) {
+        state.articles.push(action.payload)
+    }
     return state;
 }
 
